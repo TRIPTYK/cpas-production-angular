d5dedb2f83b49ff376f49825c46cfa198e0e0300
Gilles BERTRAND <gilles@triptyk.eu>
2020-12-16 19:13:22 +0100

    add all

--- libs/core-data/src/lib/services/submissions.service.ts
+++ libs/core-data/src/lib/services/submissions.service.ts
@@ -4,6 +4,31 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class SubmissionsService {
+<<<<<<< Updated upstream
+=======
+  model = 'submissions';
+  endPoint = 'http://localhost:3000/';
+  constructor(private http: HttpClient) {}
+
+  all() {
+    return this.http.get<Submission[]>(this.getUrl());
+  }
+  find(id: string) {
+    return this.http.get<Submission>(this.getUrlWithId(id));
+  }
+  create(submission: Submission) {
+    return this.http.post(this.getUrl(), submission);
+  }
+  update(submission: Submission) {
+    return this.http.put(this.getUrlWithId(submission.id), submission);
+  }
+  delete(submission: Submission) {
+    return this.http.delete(this.getUrlWithId(submission.id));
+  }
+  private getUrl() {
+    return `${this.endPoint}${this.model}`;
+  }
+>>>>>>> Stashed changes
 
   constructor() { }
 }
